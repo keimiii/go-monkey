a6244e2ad104b53f681f76fbfaf4cddd2819db2f
kaymei.leong <leongkaymei@gmail.com>
2025-07-02 14:56:39 +0800

    feat: init ast and parser

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import "go-monkey/token"
+
+type Node interface {
+	// TokenLiteral returns the literal value of the token it's associated with
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program node is going to be the root node of every AST our parser produces
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // The token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // The token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"go-monkey/ast"
+	"go-monkey/lexer"
+	"go-monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read 2 tokens, setting both curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
